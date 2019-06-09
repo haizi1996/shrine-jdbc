53e119fcb182671fb589107c68adbdb0cb71e450
haizi1996 <1051956253@qq.com>
2019-06-09 20:32:48 +0800

    删除文件

--- Keyword.java
+++
@@ -1,4 +0,0 @@
-package com.hailin.shrine.jdbc.core.parse.common.lexer.token;
-
-public interface Keyword extends TokenType {
-}
--- TokenType.java
+++
@@ -1,8 +0,0 @@
-package com.hailin.shrine.jdbc.core.parse.common.lexer.token;
-
-/**
- * token 类型
- * @author zhanghailin
- */
-public interface TokenType {
-}
